c013dc884eb0235f3d4f611ca5b76665bc4a433c
pace <143985393+ilhamfs7@users.noreply.github.com>
2024-08-22 20:25:16 +0700

    Update Soal1.java

--- Soal1.java
+++ Soal1.java
@@ -6,3 +6,10 @@ public class Soal {
         System.out.println("Hasil 1 "+ hasil);
     }
 }
+
+/**
+* Program ini merepresentasikan penjumlahan matematika/aritmatika pada bahasa program java dengan menggunakan tipe data Byte.
+* Ketika program ini dijalankan, output yang diberikan akan tidak sesuai dengan apa yang kita harapkan.
+* Output yang diharapkan adalah menghasilkan angka 131, tetapi output yang diberikan ketika program ini dijalankan adalah -125.
+* Output -125, itu dikarenakan operasi penjumlahan matematika/aritmatika pada program tersebut melebihi ukuran/range maksimal yang dimiliki oleh tipe data Byte, dimana range/ukuran dari tipe data Byte dimulai dari -128 hingga 127.
+*/
