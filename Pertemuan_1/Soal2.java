6c9aa3b2fc1cef7b449af35177f28112a47bb510
pace <143985393+ilhamfs7@users.noreply.github.com>
2024-09-07 16:59:45 +0700

    Add files via upload

---
+++ Pertemuan_1/Soal2.java
@@ -0,0 +1,8 @@
+public class Soal2 {
+    public static void main(String[] args) {
+        int i = 42;
+        String s = (i < 40)?"Life":(i > 50)?"Universe":"Everything";
+        System.out.println(s);
+    }
+
+}
